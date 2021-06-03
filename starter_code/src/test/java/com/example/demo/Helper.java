94d3a740d0a95ef330891c9e60ba5cccb24900a3
jmba <jm.bayerl@googlemail.com>
2021-06-03 13:00:22 +0200

    Added tests for chart controller.

--- starter_code/src/test/java/com/example/demo/Helper.java
+++ starter_code/src/test/java/com/example/demo/Helper.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
 import com.example.demo.model.requests.CreateUserRequest;
+import com.example.demo.model.requests.ModifyCartRequest;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Helper {
-
-
     public static void injectObjects(Object target, String fieldName, Object toInject){
         boolean wasPrivate = false;
         try {
@@ -30,5 +34,39 @@ public class Helper {
         userRequest.setPassword(password);
         userRequest.setConfirmPassword(password);
         return userRequest;
+    }
+
+    public static User getDummyUser(long userId, String username, String password, Cart cart) {
+        User newUser = new User();
+        newUser.setId(userId);
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+        newUser.setCart(cart);
+        return newUser;
+    }
+
+    public static ModifyCartRequest getDummyCartRequest(long itemId, int quantity, String username) {
+        ModifyCartRequest cartRequest = new ModifyCartRequest();
+        cartRequest.setItemId(itemId);
+        cartRequest.setQuantity(quantity);
+        cartRequest.setUsername(username);
+        return cartRequest;
+    }
+
+    public static Item getDummyItem(Long id, String name, BigDecimal price, String description) {
+        Item item = new Item();
+        item.setId(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setDescription(description);
+        return item;
+    }
+
+    public static Cart getDummyCart(long cartId, ArrayList<Item> items, User user) {
+        Cart cart = new Cart();
+        cart.setId(cartId);
+        cart.setItems(items);
+        cart.setUser(user);
+        return cart;
     }
 }
--- starter_code/src/test/java/com/example/demo/controller/CartControllerTest.java
+++ starter_code/src/test/java/com/example/demo/controller/CartControllerTest.java
@@ -5,38 +5,85 @@ import com.example.demo.controllers.CartController;
 import com.example.demo.controllers.ItemController;
 import com.example.demo.controllers.OrderController;
 import com.example.demo.controllers.UserController;
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.ItemRepository;
-import com.example.demo.model.persistence.repositories.OrderRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import static org.mockito.Mockito.mock;
 
 public class CartControllerTest {
     private CartController cartController;
-    private ItemController itemController;
-    private OrderController orderController;
-    private UserController userController;
-
+    private UserRepository userRepository = mock(UserRepository.class);
     private CartRepository cartRepository = mock(CartRepository.class);
     private ItemRepository itemRepository = mock(ItemRepository.class);
-    private OrderRepository orderRepository = mock(OrderRepository.class);
-    private UserRepository userRepository = mock(UserRepository.class);
-
     private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+    String userName = "Johnson";
+    String password = "password123";
+    String encodedPassword = "encodedPwassword";
+
+    String itemName = "item1";
+    BigDecimal price = new BigDecimal("22");
+    String description = "description";
+    int quantity = 5;
 
     @Before
     public void init() {
+        cartController = new CartController();
         Helper.injectObjects(cartController, "userRepository", userRepository);
         Helper.injectObjects(cartController, "cartRepository", cartRepository);
         Helper.injectObjects(cartController, "itemRepository", itemRepository);
-        Helper.injectObjects(itemController, "itemRepository", itemRepository);
-        Helper.injectObjects(orderController, "userRepository", userRepository);
-        Helper.injectObjects(orderController, "orderRepository", orderRepository);
-        Helper.injectObjects(userController, "cartRepository", cartRepository);
-        Helper.injectObjects(userController, "userRepository", userRepository);
-        Helper.injectObjects(userController, "bCryptPasswordEncoder", encoder);
+    }
+
+    @Test
+    public void addToCartTest() {
+        User user1 = Helper.getDummyUser(1l, userName, password, new Cart());
+        Item item1 = Helper.getDummyItem(1L, itemName, price, description);
+        Mockito.when(userRepository.findByUsername(userName)).thenReturn(user1);
+        Mockito.when(itemRepository.findById(1L)).thenReturn(Optional.of(item1));
+
+        ModifyCartRequest newCartRequest = Helper.getDummyCartRequest(1L, quantity, userName);
+        ArrayList<Item> listOfItems = new ArrayList<Item>();
+        listOfItems.add(item1);
+
+        ResponseEntity<Cart> response = cartController.addTocart(newCartRequest);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        Cart cart = response.getBody();
+        assertNotNull(cart);
+        assertEquals(quantity, cart.getItems().size());
+    }
+
+    @Test
+    public void removeFromCartTest() {
+        User user1 = Helper.getDummyUser(1l, userName, password, new Cart());
+        Item item1 = Helper.getDummyItem(1L, itemName, price, description);
+        Mockito.when(userRepository.findByUsername(userName)).thenReturn(user1);
+        Mockito.when(itemRepository.findById(1L)).thenReturn(Optional.of(item1));
+
+        ModifyCartRequest cartRequest = Helper.getDummyCartRequest(1L, quantity, userName);
+        ResponseEntity<Cart> response = cartController.removeFromcart(cartRequest);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        Cart cart = response.getBody();
+        assertNotNull(cart);
+        assertEquals(0, cart.getItems().size());
     }
 }
